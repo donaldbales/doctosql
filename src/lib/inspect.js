aec885b9d3a9e942f1eba278befa6c1d71f39e44
Donald Bales <don@donaldbales.com>
2021-04-05 15:28:41 -0700

    Use typescript 4.

--- src/lib/inspect.js
+++ src/lib/inspect.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.inspect = void 0;
 const util = require("util");
 function inspect(obj, depth = 13) {
     return `${util.inspect(obj, true, depth, false)}`;
 }
 exports.inspect = inspect;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5zcGVjdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluc3BlY3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSw2QkFBNkI7QUFFN0IsaUJBQXdCLEdBQVEsRUFBRSxRQUFnQixFQUFFO0lBQ2xELE9BQU8sR0FBRyxJQUFJLENBQUMsT0FBTyxDQUFDLEdBQUcsRUFBRSxJQUFJLEVBQUUsS0FBSyxFQUFFLEtBQUssQ0FBQyxFQUFFLENBQUM7QUFDcEQsQ0FBQztBQUZELDBCQUVDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5zcGVjdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluc3BlY3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsNkJBQTZCO0FBRTdCLFNBQWdCLE9BQU8sQ0FBQyxHQUFRLEVBQUUsUUFBZ0IsRUFBRTtJQUNsRCxPQUFPLEdBQUcsSUFBSSxDQUFDLE9BQU8sQ0FBQyxHQUFHLEVBQUUsSUFBSSxFQUFFLEtBQUssRUFBRSxLQUFLLENBQUMsRUFBRSxDQUFDO0FBQ3BELENBQUM7QUFGRCwwQkFFQyJ9
